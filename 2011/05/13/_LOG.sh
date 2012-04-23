bb05893263bec0cea9ad8fa6b03a6c036fe9dd4e
Stuart Andrews <stu.andrews@gmail.com>
2012-04-23 17:10:19 -0400

    Repo cleanup - keeping minimal set of files
    - Excluding _YYYY-MM-DD-Draft.md
    - Excluding _LOG.sh files
    - Excluding tmp image files

--- 2011/05/13/_LOG.sh
+++
@@ -1,6 +0,0 @@
-pushd /Users/stu/Desktop/Dropbox/Documents/TrunkNotes/tub78.github.com/2011/05/13
-
-cp /Users/stu/Downloads/Andrews@Andrews.gif .
-
-convert Andrews@Andrews.gif -resize 340x340  Andrews@Andrews.jpg
-
