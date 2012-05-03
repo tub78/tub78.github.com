cb259d239d7c3ac4e0094cce8c35300cff7f2fff
Stuart Andrews <stu.andrews@gmail.com>
2012-05-03 13:26:43 -0400

    M  _jbuild.sh

--- _jbuild.sh
+++ _jbuild.sh
@@ -8,17 +8,17 @@ pushd "$TRUNKNOTES/stuartjandrews.com/"
 
 # Checkout source branch
 git checkout source
-[ $? -eq 0 ] || exit;
+[ $? -eq 0 ] || { echo "Checkout Source Failed"; exit; };
 
 # Commit and push
 git add -A
 git commit -m "$(git status -s)"
 git push origin source
-[ $? -eq 0 ] || exit;
+[ $? -eq 0 ] || { echo "Push Failed"; exit; };
 
 # Build site
 mkdir -p .site
 bin/jekyll --no-auto .site
-[ $? -eq 0 ] || exit;
+[ $? -eq 0 ] || { echo "Jekyll Failed"; exit; };
 
 echo 'Build complete!'
