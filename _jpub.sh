d6fa9f578ffb376dc328bfe44182c013a1fc4326
Stuart Andrews <stu.andrews@gmail.com>
2012-05-03 22:12:33 -0400

    Update publish script

--- _jpub.sh
+++ _jpub.sh
@@ -4,13 +4,11 @@
 # A smaller repo with symlinks does not help publish speed
 
 BLOG="$TRUNKNOTES/stuartjandrews.com"
-BLOGPUB="$BLOG"-1
+BLOGPUB="$BLOG"-0
 
-
-# Clone blog
+# Clone blog locally 
 git clone "$BLOG" "$BLOGPUB"
 pushd "$BLOGPUB"
-#git remote add origin git@github.com:tub78/tub78.github.com.git
 
 # Checkout master branch
 git checkout master
@@ -19,35 +17,23 @@ git checkout master
 # Wipe-clean repo, except dot files
 rm -rf *
 
-# Copy site folder 
+# Copy site folder & commit
 cp -r "$BLOG"/_site/* .
-
-# Display changes
-git status -s
-
-# Commit and push
 git add -A
 git commit -m "$(git status -s)"
+
+# Push locally
 git push origin master
+[ $? -eq 0 ] || { echo "Push Failed"; exit; };
+
+# Create additional GH remote & push there too
+git remote add origin0 git@github.com:tub78/tub78.github.com.git
+git push origin0 master
 [ $? -eq 0 ] || { echo "Push Failed"; exit; };
 
 # Clean-up
 popd
 rm -rf "$BLOGPUB"
 echo 'Jekyll Publish complete!'
-exit;
 
 
-# Check status
-url="tub78.github.com/$(git status -s | head -1 | cut -c4-)"
-original=$(wget -qO- $url)
-
-while test "$original" = "$(wget -qO- $url)"; do
-	for i in {1..10}; do
-		# 10 seconds of filler, 10 dots
-		echo -n '.'
-		sleep 1
-	done
-done
-
-echo 'Publish complete!'
