507de12c2d9a249d5b2578f87d59f89e422cfb3d
Stuart Andrews <stu.andrews@gmail.com>
2012-09-29 23:34:28 -0400

    M  2011/04/22/Online-Footprint.html M  2011/11/16/Low-Hanging-Fruit.html A  2012/09/24/NGS-ML-Intro.html A  2012/09/24/Netflix-latentfactors-r0.jpg A  2012/09/24/Netflix-latentfactors.jpg A  2012/09/24/VCS.sh A  2012/09/24/array-cgh-r0.jpg A  2012/09/24/array-cgh.pdf A  2012/09/24/bioinformatics1-r0.jpg A  2012/09/24/bioinformatics1.pdf A  2012/09/24/bioinformatics2-r0.jpg A  2012/09/24/bioinformatics2.pdf A  2012/09/24/decision-tree-r0.jpg A  2012/09/24/decision-tree.pdf A  2012/09/24/google-r0.jpg A  2012/09/24/google.pdf A  2012/09/24/haplotypes1-r0.jpg A  2012/09/24/haplotypes1.pdf A  2012/09/24/knn1-r0.jpg A  2012/09/24/knn1.pdf A  2012/09/24/knn15-r0.jpg A  2012/09/24/knn15.pdf A  2012/09/24/linear-svm-r0.jpg A  2012/09/24/linear-svm.pdf A  2012/09/24/microarray.jpg A  2012/09/24/microarray.pdf A  2012/09/24/mxn-matrix-r0.jpg A  2012/09/24/mxn-matrix.pdf A  2012/09/24/netflix-top10.jpg A  2012/09/24/nonlinear-svm-r0.jpg A  2012/09/24/nonlinear-svm.pdf A  2012/09/24/polyphen-r0.jpg A  2012/09/24/polyphen.jpg A  2012/09/24/polyphen2-r0.jpg A  2012/09/24/polyphen2-sunyaev-r0.jpg A  2012/09/24/polyphen2-sunyaev.jpg A  2012/09/24/polyphen2-sunyaev.pdf A  2012/09/24/polyphen2.jpg A  2012/09/24/polyphen2.pdf A  2012/09/24/protein-struct1-r0.jpg A  2012/09/24/protein-struct1.pdf A  2012/09/24/regression-r0.jpg A  2012/09/24/regression.pdf A  2012/09/24/sim.t.test.density-p-value-p1.jpg A  2012/09/24/sim.t.test.density-p-value-p2.jpg A  2012/09/24/splicing1-r0.jpg A  2012/09/24/splicing1.pdf A  2012/09/24/svm-linear-r0.jpg A  2012/09/24/svm-linear.pdf A  2012/09/24/svm-nonlinear-r0.jpg A  2012/09/24/svm-nonlinear.pdf A  Gemfile.lock M  about.html M  index.html M  pages/Vimwiki.html M  tags.html

---
+++ 2012/09/24/VCS.sh
@@ -0,0 +1,269 @@
+convert -resample 50 decision-tree.pdf     decision-tree-r0.jpg
+exit;
+convert -resample 50 linear-svm.pdf     linear-svm-r0.jpg
+convert -resample 50 nonlinear-svm.pdf  nonlinear-svm-r0.jpg
+exit;
+convert -resample 50 regression.pdf     regression-r0.jpg
+exit;
+convert -resample 50 mxn-matrix.pdf     mxn-matrix-r0.jpg
+exit;
+convert -resample 50 knn15.pdf          knn15-r0.jpg
+convert -resample 50 knn1.pdf           knn1-r0.jpg
+convert -resample 50 svm-nonlinear.pdf  svm-nonlinear-r0.jpg
+convert -resample 50 svm-linear.pdf     svm-linear-r0.jpg
+exit;
+
+convert -resample 50 google.pdf   google-r0.jpg
+exit;
+
+convert -resample 50 bioinformatics2.pdf   bioinformatics2-r0.jpg
+convert -resample 50 bioinformatics1.pdf   bioinformatics1-r0.jpg
+exit;
+convert -resample 60 haplotypes1.pdf       haplotypes1-r0.jpg
+convert -resample 60 protein-struct1.pdf   protein-struct1-r0.jpg
+convert -resample 60 splicing1.pdf         splicing1-r0.jpg
+exit;
+
+
+convert -resample 60 polyphen2-sunyaev.pdf polyphen2-sunyaev-r0.jpg
+exit
+
+convert -resample 30 polyphen2-sunyaev.pdf polyphen2-sunyaev-r0.jpg
+exit
+
+convert -resample 45 array-cgh.pdf array-cgh-r0.jpg
+convert -resample 40 polyphen2.pdf polyphen2-r0.jpg
+exit
+
+convert -resample 15 polyphen.jpg polyphen-r0.jpg
+exit
+
+convert polyphen2-sunyaev.pdf polyphen2-sunyaev.jpg
+exit
+
+
+#
+convert -resample 100 Netflix-latentfactors.jpg Netflix-latentfactors-r0.jpg
+exit;
+
+
+#
+convert -resample 75 Netflix-latentfactors.jpg Netflix-latentfactors-r0.jpg
+exit;
+
+# convert netflix-top10.jpg
+exit;
+
+convert microarray.pdf microarray.jpg
+exit;
+
+
+
+
+# selflog.sh> preventing recursion with "exit;"
+exit;
+# selflog.sh> starting log Mon Sep 24 23:20:47 EDT 2012
++ convert -resample 200 Netflix-latentfactors.jpg Netflix-latentfactors-r0.jpg
++ exit
+
+
+
+# selflog.sh> preventing recursion with "exit;"
+exit;
+# selflog.sh> starting log Mon Sep 24 23:21:54 EDT 2012
++ convert -resample 50 Netflix-latentfactors.jpg Netflix-latentfactors-r0.jpg
++ exit
+
+
+
+# selflog.sh> preventing recursion with "exit;"
+exit;
+# selflog.sh> starting log Mon Sep 24 23:30:04 EDT 2012
++ convert polyphen2.pdf polyphen2.jpg
++ exit
+
+
+
+# selflog.sh> preventing recursion with "exit;"
+exit;
+# selflog.sh> starting log Mon Sep 24 23:33:03 EDT 2012
++ convert -resample 100 polyphen2.pdf polyphen2-r0.jpg
++ convert -resample 75 Netflix-latentfactors.jpg Netflix-latentfactors-r0.jpg
++ exit
+
+
+
+# selflog.sh> preventing recursion with "exit;"
+exit;
+# selflog.sh> starting log Tue Sep 25 08:32:53 EDT 2012
++ convert -resample 100 Netflix-latentfactors.jpg Netflix-latentfactors-r0.jpg
++ exit
+
+
+
+# selflog.sh> preventing recursion with "exit;"
+exit;
+# selflog.sh> starting log Tue Sep 25 08:39:27 EDT 2012
++ convert polyphen2-sunyaev.pdf polyphen2-sunyaev.jpg
++ exit
+
+
+
+# selflog.sh> preventing recursion with "exit;"
+exit;
+# selflog.sh> starting log Tue Sep 25 08:45:01 EDT 2012
++ convert -resample 50 polyphen.jpg polyphen-r0.jpg
++ exit
+
+
+
+# selflog.sh> preventing recursion with "exit;"
+exit;
+# selflog.sh> starting log Tue Sep 25 09:06:31 EDT 2012
++ convert -resample 25 polyphen.jpg polyphen-r0.jpg
++ convert -resample 25 polyphen2.pdf polyphen2-r0.jpg
++ exit
+
+
+
+# selflog.sh> preventing recursion with "exit;"
+exit;
+# selflog.sh> starting log Tue Sep 25 09:16:17 EDT 2012
++ convert -resample 50 array-cgh.pdf array-cgh-r0.jpg
++ convert -resample 25 polyphen2.pdf polyphen2-r0.jpg
++ exit
+
+
+
+# selflog.sh> preventing recursion with "exit;"
+exit;
+# selflog.sh> starting log Tue Sep 25 09:19:14 EDT 2012
++ convert -resample 30 array-cgh.pdf array-cgh-r0.jpg
++ convert -resample 35 polyphen2.pdf polyphen2-r0.jpg
++ exit
+
+
+
+# selflog.sh> preventing recursion with "exit;"
+exit;
+# selflog.sh> starting log Tue Sep 25 09:20:58 EDT 2012
++ convert -resample 45 array-cgh.pdf array-cgh-r0.jpg
++ convert -resample 40 polyphen2.pdf polyphen2-r0.jpg
++ convert -resample 50 polyphen2-sunyaev.pdf polyphen2-sunyaev-r0.jpg
++ exit
+
+
+
+# selflog.sh> preventing recursion with "exit;"
+exit;
+# selflog.sh> starting log Tue Sep 25 09:21:58 EDT 2012
++ convert -resample 30 polyphen2-sunyaev.pdf polyphen2-sunyaev-r0.jpg
++ exit
+
+
+
+# selflog.sh> preventing recursion with "exit;"
+exit;
+# selflog.sh> starting log Tue Sep 25 09:22:29 EDT 2012
++ convert -resample 10 polyphen2-sunyaev.pdf polyphen2-sunyaev-r0.jpg
++ exit
+
+
+
+# selflog.sh> preventing recursion with "exit;"
+exit;
+# selflog.sh> starting log Tue Sep 25 09:23:27 EDT 2012
++ convert -resample 40 polyphen2-sunyaev.pdf polyphen2-sunyaev-r0.jpg
++ exit
+
+
+
+# selflog.sh> preventing recursion with "exit;"
+exit;
+# selflog.sh> starting log Tue Sep 25 09:24:18 EDT 2012
++ convert -resample 70 polyphen2-sunyaev.pdf polyphen2-sunyaev-r0.jpg
++ exit
+
+
+
+# selflog.sh> preventing recursion with "exit;"
+exit;
+# selflog.sh> starting log Tue Sep 25 09:24:48 EDT 2012
++ convert -resample 60 polyphen2-sunyaev.pdf polyphen2-sunyaev-r0.jpg
++ exit
+
+
+
+# selflog.sh> preventing recursion with "exit;"
+exit;
+# selflog.sh> starting log Tue Sep 25 09:53:12 EDT 2012
++ convert -resample 60 bioinformatics2.pdf bioinformatics2-r0.jpg
++ convert -resample 60 bioinformatics1.pdf bioinformatics1-r0.jpg
++ convert -resample 60 haplotypes1.pdf haplotypes1-r0.jpg
++ convert -resample 60 protein-struct1.pdf protein-struct1-r0.jpg
++ convert -resample 60 splicing1.pdf splicing1-r0.jpg
++ exit
+
+
+
+# selflog.sh> preventing recursion with "exit;"
+exit;
+# selflog.sh> starting log Tue Sep 25 10:03:10 EDT 2012
++ convert -resample 50 bioinformatics2.pdf bioinformatics2-r0.jpg
++ convert -resample 50 bioinformatics1.pdf bioinformatics1-r0.jpg
++ exit
+
+
+
+# selflog.sh> preventing recursion with "exit;"
+exit;
+# selflog.sh> starting log Tue Sep 25 10:07:01 EDT 2012
++ convert -resample 50 google.pdf google-r0.jpg
++ exit
+
+
+
+# selflog.sh> preventing recursion with "exit;"
+exit;
+# selflog.sh> starting log Tue Sep 25 11:28:09 EDT 2012
++ convert -resample 50 knn15.pdf knn15-r0.jpg
++ convert -resample 50 knn1.pdf knn1-r0.jpg
++ convert -resample 50 svm-nonlinear.pdf svm-nonlinear-r0.jpg
++ convert -resample 50 svm-linear.pdf svm-linear-r0.jpg
++ convert -resample 50 mxn-matrix.pdf mxn-matrix-r0.jpg
+convert: unable to open image `mxn-matrix.pdf': No such file or directory @ error/blob.c/OpenBlob/2617.
+convert: no images defined `mxn-matrix-r0.jpg' @ error/convert.c/ConvertImageCommand/3032.
++ exit
+
+
+
+# selflog.sh> preventing recursion with "exit;"
+exit;
+# selflog.sh> starting log Tue Sep 25 11:29:01 EDT 2012
++ convert -resample 50 mxn-matrix.pdf mxn-matrix-r0.jpg
++ exit
+
+
+
+# selflog.sh> preventing recursion with "exit;"
+exit;
+# selflog.sh> starting log Tue Sep 25 11:53:43 EDT 2012
++ convert -resample 50 regression.pdf regression-r0.jpg
++ exit
+
+
+
+# selflog.sh> preventing recursion with "exit;"
+exit;
+# selflog.sh> starting log Tue Sep 25 12:09:20 EDT 2012
++ convert -resample 50 linear-svm.pdf linear-svm-r0.jpg
++ convert -resample 50 nonlinear-svm.pdf nonlinear-svm-r0.jpg
++ exit
+
+
+
+# selflog.sh> preventing recursion with "exit;"
+exit;
+# selflog.sh> starting log Tue Sep 25 12:14:40 EDT 2012
++ convert -resample 50 decision-tree.pdf decision-tree-r0.jpg
++ exit
